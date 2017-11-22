62f5e89acba57b4460f05fa301a0b1ae28b1231d
catalin <lazcatluc@gmail.com>
2017-11-22 07:01:55 +0200

    Improve performance

--- src/main/java/ro/contezi/log/reader/Main.java
+++ src/main/java/ro/contezi/log/reader/Main.java
@@ -8,12 +8,25 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     
     public static void main(String[] args) throws IOException {
-        int linesToGenerate = args.length > 0 ? Integer.parseInt(args[0]) : 6000000;
-        
-        LOGGER.log(Level.INFO, "Generating " + linesToGenerate + " lines");
-        String file = "target/generated.log";
-        new Generator.Builder().lines(linesToGenerate).uri("/a").uri("/b").uri("/a/c").uri("/a/c/d").file(file).build().generate();
-        LOGGER.log(Level.INFO, "Parsing file");
+    	String file;
+    	int linesParam;
+    	try {
+    		linesParam = Integer.parseInt(args[0]);
+    	}
+    	catch (RuntimeException re) {
+    		linesParam = -1;
+    	}
+    	if (args.length == 0 || linesParam > -1) {
+    		int linesToGenerate = linesParam > -1 ? linesParam : 6000000;
+    		file = args.length == 1 ? "target/generated.log" : args[1];
+    		LOGGER.log(Level.INFO, "Generating " + linesToGenerate + " lines");
+    		new Generator.Builder().lines(linesToGenerate).uri("/a").uri("/b")
+    				.uri("/a/c").uri("/a/c/d").file(file).build().generate();
+        }
+        else {
+        	file = args[0];
+    	}
+        LOGGER.log(Level.INFO, "Parsing file " + file);
         LOGGER.log(Level.INFO, new Parser(file).getRequestCount().toString());
     }
 }
--- src/main/java/ro/contezi/log/reader/Parser.java
+++ src/main/java/ro/contezi/log/reader/Parser.java
@@ -5,20 +5,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Parser {
-    private final Statistic[] statistics;
     private final Map<String, Integer> requestCount;
     
     public Parser(String file) throws IOException {
+        requestCount = new ConcurrentHashMap<>();
         try (BufferedReader reader = Files.newBufferedReader(new File(file).toPath(), Charset.forName("UTF-8"))) {
-            statistics = reader.lines().map(Statistic::new).toArray(i -> new Statistic[i]);
-        }
-        requestCount = new HashMap<>();
-        for (Statistic statistic : statistics) {
-            requestCount.compute(statistic.getRequest(), (s, i) -> i == null ? 1 : i + 1);
+        	reader.lines().parallel().map(Statistic::new).forEach(statistic -> 
+        		requestCount.compute(statistic.getRequest(), (s, i) -> i == null ? 1 : i + 1));
         }
     }
 
